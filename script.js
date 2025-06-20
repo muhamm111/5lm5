5a4f4c7748535c3e2f5dabe6db0b054630ea6798
muhamm111 <muhammadaminulugbekov20@gmail.com>
2025-06-20 20:24:59 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+let userName = prompt('Ismingizni kiriting')
+let userId = prompt('Space Idisini kiriting')
+let userPassword = prompt('Space paswordingizni kiriting')
+let user = {
+    //key : value;
+    name: userName,
+    id: userId,
+    password: userPassword,
+}
+
+console.log(user);
